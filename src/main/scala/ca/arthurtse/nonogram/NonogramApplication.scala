6ac43a19c513fdf63d38351376a28e172cd253ad
Arthur Tse <17014719+arty-tse@users.noreply.github.com>
2019-10-23 02:22:38 -0700

    Begin styling

--- src/main/scala/ca/arthurtse/nonogram/NonogramApplication.scala
+++ src/main/scala/ca/arthurtse/nonogram/NonogramApplication.scala
@@ -4,6 +4,7 @@ import ca.arthurtse.nonogram.model.PuzzleData.Puzzle
 import ca.arthurtse.nonogram.model._
 import ca.arthurtse.nonogram.view.{GridView, MenuView}
 import com.squareup.moshi.Moshi
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
@@ -11,22 +12,21 @@ import scalafx.scene.Scene
 import scala.io._
 
 object NonogramApplication extends JFXApp {
+  private val stylesheetName: String = "/styles.css"
+  JFXApp.userAgentStylesheet = getClass.getResource(stylesheetName).toExternalForm
+
   private val fileName: String = "/puzzle_data.json"
   private val source: BufferedSource = Source.fromInputStream(getClass.getResourceAsStream(fileName))
   private val json: String = try source.getLines.mkString finally source.close
   private val puzzleData: PuzzleData = new Moshi.Builder().build().adapter(classOf[PuzzleData]).fromJson(json)
 
   private val menuView = new MenuView(puzzleData, openPuzzle)
-  private val menuScene = new Scene {
-    content = menuView.view
-  }
+  private val menuScene = new Scene(menuView.view)
 
   private def openPuzzle(puzzle: Puzzle): Unit = {
     val gridModel = new GridModel(puzzle, puzzleData.legend)
     val gridView = new GridView(gridModel, returnToMenu)
-    stage.scene = new Scene {
-      content = gridView.view
-    }
+    stage.scene = new Scene(gridView.view, 600, 600)
   }
 
   private def returnToMenu(): Unit = {
@@ -35,8 +35,8 @@ object NonogramApplication extends JFXApp {
 
   stage = new PrimaryStage {
     title = "Nonogram Puzzler"
-    width = 400
-    height = 400
+    width = 600
+    height = 600
     scene = menuScene
   }
 }
--- src/main/scala/ca/arthurtse/nonogram/view/MenuView.scala
+++ src/main/scala/ca/arthurtse/nonogram/view/MenuView.scala
@@ -11,18 +11,19 @@ import scalafx.scene.shape.Rectangle
 import scalafx.scene.text.Text
 
 class MenuView(puzzleData: PuzzleData, goToPuzzle: Puzzle => Unit) {
-  val view: Pane = new FlowPane(Horizontal, 10, 10) {
-    prefWrapLength = 280
-    padding = Insets(10)
-    style = "-fx-background-color: mintcream"
+  val view: Pane = new FlowPane {
+    id = "menu-pane"
+    prefWrapLength = 600
   }
   for (puzzle <- puzzleData.puzzles) {
     val img = new Rectangle {
-      width = 80
-      height = 80
-      fill = Chartreuse
+      width = 150
+      height = 150
+      styleClass += "puzzle-pic"
     }
-    val name = new Text(puzzle.name)
+    val name = new Text(puzzle.name) {
+      styleClass += "puzzle-text"
+    }
     val item = new VBox {
       spacing = 10
       alignment = Center
